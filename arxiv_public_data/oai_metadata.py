8476caea34e04e1ddb357d73148b1b4369eb9b6b
Dmitry Morozov <dvmorozov@hotmail.com>
2022-09-06 16:25:25 +0200

    Terminating bracket has been added.

--- arxiv_public_data/oai_metadata.py
+++ arxiv_public_data/oai_metadata.py
@@ -258,7 +258,7 @@ def all_of_arxiv(outfile=None, resumptionToken=None, autoresume=True):
             else:
                 log.info('No resumption token, query finished')
                 with gzip.open(outfile, 'at', encoding='utf-8') as fout:
-                    fout.write(']\n')
+                    fout.write(']}\n')
                 return
 
             time.sleep(12)  # OAI server usually requires a 10s wait
